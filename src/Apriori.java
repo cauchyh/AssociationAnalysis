2d5cd832b69f45ab1d5e44d96ee41fe64da61371
cauchyh <cauchy.huang@gmail.com>
2014-06-20 17:43:42 -0400

    New Update

--- src/Apriori.java
+++ src/Apriori.java
@@ -2,18 +2,50 @@
 import java.util.*;
 public class Apriori 
 {
-	public List<List<Integer>> frequentItemSet(List<Integer> itemSet, double minSup)
+	public List<List<Integer>> frequentItemSet(List<List<List<Integer>>> itemSet, double minSup)
 	{
 		// The input itemSet is supposed to be different
-		int totalItemCount = itemSet.size();
+		// It is lists that look like [ [[1],[2],[3]], [[2],[3],[4]],...]
+		int totalItemCount = countTotalItem(itemSet);
 		int minSupItemCount = (int) (minSup*totalItemCount);
 		List<List<Integer>> frequentItemSet = new ArrayList<List<Integer>> ();
-		
+		for ()
 		return frequentItemSet;
 	}
 	
 	private List<Integer> generateSubsets(List<Integer> itemSet, int subsetLength)
 	{
+		List<Integer> subsets = new ArrayList<Integer>();
 		
+		return subsets;
 	}
+	
+	private int countTotalItem(List<List<List<Integer>>> itemSet)
+	{
+		int totalItem = 0;
+		// Gain the total item number form every set
+		for (List<List<Integer>> list : itemSet) {
+			totalItem += list.size();
+		}
+		return totalItem;
+	}
+	
+	// Method that is to check the total count of each item
+	public Map<List<Integer>, Integer> countItemSet(List<List<List<Integer>>> itemSet)
+	{ 
+		Map<List<Integer>, Integer> itemCountMap = new HashMap<List<Integer>, Integer>();
+		// Iterate from each item set record
+		for (List<List<Integer>> eachSet : itemSet) {
+			for (List<Integer> eachItem : eachSet) {
+				if (itemCountMap.containsKey(eachItem)) {
+					int newCount = itemCountMap.get(eachItem) + 1;
+					itemCountMap.put(eachItem, newCount);
+				} else {
+					itemCountMap.put(eachItem, 1);
+				}
+			}
+		}
+		return itemCountMap;
+	}
+	
 }
--- src/test.java
+++ src/test.java
@@ -1,9 +1,34 @@
-
+import java.util.*;
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		ArrayList<List<List<Integer>>> test1 = new ArrayList<List<List<Integer>>> ();
+		ArrayList<List<Integer>> l1 = new ArrayList<List<Integer>>();
+		ArrayList<Integer> i1 = new ArrayList<Integer>();
+		i1.add(1);
+		for (int i=0; i<5; i++) {
+			l1.add(i1);
+			i1 = new ArrayList<Integer>();
+			i1.add(i);
+		}
+		ArrayList<List<Integer>> l2 = new ArrayList<List<Integer>>();
+		ArrayList<Integer> i2 = new ArrayList<Integer>();
+		i2.add(1);
+		for (int i=3; i<8; i++) {
+			l2.add(i2);
+			i2 = new ArrayList<Integer>();
+			i2.add(i);
+		}
+		Apriori ap1 = new Apriori();
+		test1.add(l1);
+		test1.add(l2);
+//		System.out.println(l1);
+//		System.out.println(l2);
+		Map<List<Integer>, Integer> res = ap1.countItemSet(test1);
+		System.out.println(res);
+		
+		
 	}
 
 }
